430f2a161bdea0d18bee114e382d620776b40937
Andrew <andrewmbyrd@gmail.com>
2016-11-29 15:20:06 -0600

    added comments

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -37,6 +37,7 @@ require_relative '../models/address_book'
          system "clear"
          read_csv
          main_menu
+      #moved goodbye down to 6 and added the new function at 5
       when 5
          system "clear"
          select_by_index
@@ -84,14 +85,20 @@ require_relative '../models/address_book'
 
   def read_csv
   end
+  #this is the new function
   def select_by_index
+    #introductory messages prompts the user to enter an entry number
     puts "Please select the number of the entry you would like to view.\nThere are currently #{@address_book.entries.count} entries.\nPlease input a NUMBER from 1 to #{@address_book.entries.count}."
+    #this gets user input and converts it to an int. non numbers are 0 after the call to to_i
     index = gets.chomp
     index = index.to_i
+    #this will reset the prompt if invalid entry is given
     unless index <= @address_book.entries.count && index>0
       puts "That is not a valid input. Please try again"
+      gets
       system "clear"
       select_by_index
+    #this codes is assuming the average user won't know that arrrays start at 0. So we do index -1 to get the correct position in the array
     else
       puts "\nHere is your requested entry. Press any key to continue:\n\n"
       puts @address_book.entries[index-1].to_s
